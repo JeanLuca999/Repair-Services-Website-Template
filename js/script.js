9ea79f1b0fb539fc7d108fb80fd93f85276bef5f
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-06-23 15:35:55 -0300

    creating section "Our services"

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 const menuButton = document.getElementsByClassName('menu')[0]
 const menuItems = document.getElementById('menu-items')
+const links = document.querySelectorAll('.link')
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('menu-clicked')
     menuItems.classList.toggle('active')
+    document.body.classList.toggle('body-overflow-hidden')
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menuButton.classList.toggle('menu-clicked')
+        menuItems.classList.toggle('active')
+        document.body.classList.remove('body-overflow-hidden')
+    })
 })
